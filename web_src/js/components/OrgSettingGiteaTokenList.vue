c1fdc687295bb2d4b6db749d92daf918f9511067
webplusai <smupyknight@outlook.com>
2023-12-26 20:22:57 +0100

    Cleanup and refactor functions

--- web_src/js/components/OrgSettingGiteaTokenList.vue
+++ web_src/js/components/OrgSettingGiteaTokenList.vue
@@ -74,20 +74,23 @@ export function initOrgSettingGiteaTokenList(){
             </button>
           </form>
         </div>
-
       </div>
-
-<!--      Modal to delete token-->
-      <div class="ui modal" id="token-delete-modal">
-        <div class="header">Gitea Token Delete</div>
-        <div class="content">
-          <p>Are you sure to delete this token?</p>
-        </div>
-        <div class="actions">
-          <div class="ui positive button">Delete</div>
-          <div class="ui negative button">Cancel</div>
-        </div>
-      </div>
-
     </div>
+
+    <!--      Modal to delete token-->
+    <div class="ui modal" id="token-delete-modal">
+      <div class="header">Gitea Token Delete</div>
+      <div class="content">
+        <p>Are you sure to delete this token?</p>
+      </div>
+      <div class="actions">
+        <div class="ui positive button">Delete</div>
+        <div class="ui negative button">Cancel</div>
+    </div>
+  </div>
 </template>
+<style scoped>
+.flex-list > .flex-item:only-child {
+  padding-bottom: 0;
+}
+</style>
