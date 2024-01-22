daa9dc7214173374521555444c6f9288737b3354
smupyknight <smupyknight@outlook.com>
2024-01-22 10:35:01 +0000

    DHS-19 implement live update of compute process

--- modules/devpod/core.go
+++ modules/devpod/core.go
@@ -1,15 +1,19 @@
 package devpod
 
 import (
+	"bufio"
 	"bytes"
-	org_model "code.gitea.io/gitea/models/organization"
-	"code.gitea.io/gitea/modules/log"
 	"fmt"
-	"github.com/rs/xid"
+	"io"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
+
+	org_model "code.gitea.io/gitea/models/organization"
+	"code.gitea.io/gitea/modules/log"
+	"github.com/rs/xid"
 )
 
 // TODO: 1. need to forward S3 credentials
@@ -18,7 +22,9 @@ import (
 
 func Execute(privateKey, user, host string, port int32,
 	gitUrl, gitUser, gitEmail string,
-	config map[string][]org_model.OrgDevpodCredential) (string, error) {
+	config map[string][]org_model.OrgDevpodCredential,
+	sendStream func(string),
+) error {
 
 	//gitUrl := "git@gitlab.com:grz1/aqua-research.git" // NOTE: make sure to trim, will get surprise if there is space
 
@@ -46,50 +52,46 @@ func Execute(privateKey, user, host string, port int32,
 		}
 	}()
 
-	var result string
-
 	// devpod provider add ssh --name <provider-id> -o HOST=vagrant@localhost -o PORT=2222 -o EXTRA_FLAGS="-i /tmp/private_key"
 	cmd := exec.Command("devpod", "provider", "add", "ssh", "--name", providerId,
 		"-o", "HOST="+user+"@"+host, "-o", "PORT="+strconv.Itoa(int(port)),
 		"-o", "EXTRA_FLAGS=-i "+privateKeyFile) // Don't use \" in EXTRA_FLAGS for some reason it not working
 	cmd.Env = os.Environ()
 	cmd.Dir = "/tmp"
-	output, err := cmd.CombinedOutput()
+
+	err = getOutputCommand(cmd, sendStream)
 	if err != nil {
-		log.Error("error when devpod ssh provider add: %v", err)
+		log.Error("Failing to add provider: %v", err)
 	}
-	result += string(output) + "\n"
 
 	// devpod up --provider <provider-id> <git-url> --ide none --debug --id <workspace-id>
 	cmd = exec.Command("devpod", "up", gitUrl, "--provider", providerId, "--ide", "none", "--id", workSpaceId)
-	output, err = cmd.CombinedOutput()
+	err = getOutputCommand(cmd, sendStream)
 	if err != nil {
-		log.Error("error when devpod up: %v", err)
+		log.Error("Failing to up devpod: %v", err)
 	}
-	result += string(output) + "\n"
 
 	for key, v := range config {
 		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "echo ['remote \""+key+"\"'] >> .dvc/config.local")
-		output, err = cmd.CombinedOutput()
+		err := getOutputCommand(cmd, sendStream)
 		if err != nil {
-			log.Error("error: %v", err)
+			log.Error("Failing to add remote: %v", err)
 		}
 		for _, value := range v {
 			cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "echo "+value.Key+"="+value.Value+" >> .dvc/config.local")
-			output, err = cmd.CombinedOutput()
+			err := getOutputCommand(cmd, sendStream)
 			if err != nil {
-				log.Error("error: %v", err)
+				log.Error("Failing to add remote: %v", err)
 			}
 		}
 	}
 
 	cmd = exec.Command("devpod", "ssh", workSpaceId,
 		"--command", "export DEVPOD_WORKSPACE_ID="+workSpaceId)
-	output, err = cmd.CombinedOutput()
+	err = getOutputCommand(cmd, sendStream)
 	if err != nil {
-		log.Error("error when export workspace id: %v", err)
+		log.Error("Failing to add remote: %v", err)
 	}
-	result += string(output) + "\n"
 
 	cmd = exec.Command("devpod", "ssh", workSpaceId,
 		"--command", "cat dvc-script.sh")
@@ -104,94 +106,132 @@ func Execute(privateKey, user, host string, port int32,
 		//devpod ssh <workspace-id> --command '[ -f dvc-script.sh ] && chmod +x dvc-script.sh && ./dvc-script.sh'
 		cmd = exec.Command("devpod", "ssh", workSpaceId,
 			"--command", "[ -f dvc-script.sh ] && chmod +x dvc-script.sh && bash dvc-script.sh")
-		output, err = cmd.CombinedOutput()
+		err = getOutputCommand(cmd, sendStream)
 		if err != nil {
-			log.Error("error when dvc-script.sh run: %v", err)
+			log.Error("Failing to add remote: %v", err)
 		}
-		result += string(output) + "\n"
 	} else {
 
 		//devpod ssh <workspace-id> --command 'dvc pull'
 		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "dvc pull")
-		output, err = cmd.CombinedOutput()
+		err = getOutputCommand(cmd, sendStream)
 		if err != nil {
-			log.Error("error when dvc pull: %v", err)
+			log.Error("Failing to add remote: %v", err)
 		}
-		result += string(output) + "\n"
 
 		//devpod ssh <workspace-id> --command 'dvc exp run'
 		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "dvc exp run")
-		output, err = cmd.CombinedOutput()
+		err = getOutputCommand(cmd, sendStream)
 		if err != nil {
-			log.Error("error when dvc exp run: %v", err)
+			log.Error("Failing to add remote: %v", err)
 		}
-		result += string(output) + "\n"
 
-		//devpod ssh <workspace-id> --command 'git add .'
-		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git add .")
-		output, err = cmd.CombinedOutput()
-		if err != nil {
-			log.Error("error when git add: %v", err)
-		}
-		result += string(output) + "\n"
+	}
 
-		//devpod ssh <workspace-id> --command 'git config user.name xxx'
-		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git config user.name "+gitUser)
-		output, err = cmd.CombinedOutput()
-		if err != nil {
-			log.Error("error when git git config user.name: %v", err)
-		}
-		result += string(output) + "\n"
+	//devpod ssh <workspace-id> --command 'git add .'
+	cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git add .")
+	err = getOutputCommand(cmd, sendStream)
+	if err != nil {
+		log.Error("Failing to add remote: %v", err)
+	}
 
-		//devpod ssh <workspace-id> --command 'git config user.email xxx'
-		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git config user.email "+gitEmail)
-		output, err = cmd.CombinedOutput()
-		if err != nil {
-			log.Error("error when git git config user.email: %v", err)
-		}
-		result += string(output) + "\n"
+	//devpod ssh <workspace-id> --command 'git config user.name xxx'
+	cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git config user.name "+gitUser)
+	if err := cmd.Run(); err != nil {
+		log.Error("Failing to add remote: %v", err)
+	}
 
-		//devpod ssh <workspace-id> --command 'git commit -m "exp run result"'
-		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git commit -m 'exp run result'")
-		output, err = cmd.CombinedOutput()
-		if err != nil {
-			log.Error("error when git commit: %v", err)
-		}
-		result += string(output) + "\n"
+	//devpod ssh <workspace-id> --command 'git config user.email xxx'
+	cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git config user.email "+gitEmail)
+	if err = getOutputCommand(cmd, sendStream); err != nil {
+		log.Error("Failing to add remote: %v", err)
+	}
 
-		//devpod ssh <workspace-id> --command 'git push origin'
-		cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git push origin")
-		output, err = cmd.CombinedOutput()
-		if err != nil {
-			log.Error("error when git push: %v", err)
-		}
-		result += string(output) + "\n"
+	//devpod ssh <workspace-id> --command 'git commit -m "exp run result"'
+	cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git commit -m 'exp run result'")
+	if err = getOutputCommand(cmd, sendStream); err != nil {
+		log.Error("Failing to add remote: %v", err)
+	}
+
+	//devpod ssh <workspace-id> --command 'git push origin'
+	cmd = exec.Command("devpod", "ssh", workSpaceId, "--command", "git push origin")
+	if err = getOutputCommand(cmd, sendStream); err != nil {
+		log.Error("Failing to add remote: %v", err)
 	}
 
 	//devpod stop <workspace-id>
 	cmd = exec.Command("devpod", "stop", workSpaceId)
-	output, err = cmd.CombinedOutput()
+	err = getOutputCommand(cmd, sendStream)
 	if err != nil {
-		log.Error("error when devpod stop: %v", err)
+		log.Error("Failing to add remote: %v", err)
 	}
-	result += string(output) + "\n"
 
 	//devpod delete <workspace-id>
 	cmd = exec.Command("devpod", "delete", workSpaceId)
-	output, err = cmd.CombinedOutput()
+	err = getOutputCommand(cmd, sendStream)
 	if err != nil {
-		log.Error("error when devpod delete: %v", err)
+		log.Error("Failing to add remote: %v", err)
 	}
-	result += string(output) + "\n"
 
 	// devpod provider delete <provider-id>
 	cmd = exec.Command("devpod", "provider", "delete", providerId)
-	//cmd.Dir = tempRepoPath
-	output, err = cmd.CombinedOutput()
+	err = getOutputCommand(cmd, sendStream)
 	if err != nil {
-		log.Error("error when devpod ssh provider remove: %v", err)
+		log.Error("Failing to add provider: %v", err)
 	}
-	result += string(output) + "\n"
 
-	return result, err
+	return err
+}
+
+// getOutputCommand is a helper function to get the stdout and stderr of a command and send it to the client
+func getOutputCommand(cmd *exec.Cmd, sendStream func(string)) error {
+
+	// Create a pipe for reading the command's output
+	outReader, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Error("Error creating StdoutPipe for Cmd: %v", err)
+		return err
+	}
+
+	// Create a pipe for reading the command's stderr
+	errReader, err := cmd.StderrPipe()
+	if err != nil {
+		log.Error("Error creating StderrPipe for Cmd: %v", err)
+		return err
+	}
+
+	// Start the command
+	err = cmd.Start()
+	if err != nil {
+		log.Error("Error starting Cmd: %v", err)
+		return err
+	}
+
+	cmdReader := io.MultiReader(outReader, errReader)
+
+	// Create a scanner to read the output
+	scanner := bufio.NewScanner(cmdReader)
+	scanner.Split(bufio.ScanLines)
+
+	// Continuously send updates to the client
+	for scanner.Scan() {
+		// Send the output to the client as an SSE message
+		sendStream(scanner.Text())
+		time.Sleep(10 * time.Millisecond) // Add a delay to streaming
+	}
+
+	// get any errors encountered during scanning
+	if err := scanner.Err(); err != nil {
+		log.Error("Error with scanner: %v", err)
+		return err
+	}
+
+	// Wait for the command to finish
+	err = cmd.Wait()
+	if err != nil {
+		log.Error("Error waiting for Cmd: %v", err)
+		return err
+	}
+
+	return nil
 }
