07c35a2f5baa7a49fa61bb89055b1cd506da2f98
smupyknight <smupyknight@outlook.com>
2024-04-11 14:01:57 +0000

    DHS-23 update template repo to have main function

---
+++ services/file/templateRepo/aqua-demo/main.py
@@ -0,0 +1,24 @@
+from dvclive import Live
+
+
+if __name__=="__main__": 
+    datapoints = [
+        {"name": "petal_width", "importance": 0.4},
+        {"name": "petal_length", "importance": 0.33},
+        {"name": "sepal_width", "importance": 0.24},
+        {"name": "sepal_length", "importance": 0.03}
+    ]
+    with Live() as live:
+        live.log_param("myParam", 123)
+        live.log_metric("myMetric", 543)
+
+        live.log_plot(
+            "iris_feature_importance",
+            datapoints,
+            x="importance",
+            y="name",
+            template="bar_horizontal",
+            title="Iris Dataset: Feature Importance",
+            y_label="Feature Name",
+            x_label="Feature Importance"
+        )
